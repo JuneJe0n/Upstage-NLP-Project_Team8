d362418476cbcd611b057d9e3ecaa336d21f3235
doellomdoel <140289496+Tikkeulmoatikkeul@users.noreply.github.com>
2024-11-21 20:17:42 +0900

    Update test_samples_check.py
    가시성을 위하여 수정
    10번 질문을 입력하여 함수를 연속적으로 사용하는 과정을 나타냄

--- wikipedia_search/test_samples_check.py
+++ wikipedia_search/test_samples_check.py
@@ -9,58 +9,47 @@ from utils_wiki import remove_html_tags, get_wikipedia_articles, parse_wikipedia
 
 
 prompts, answers = read_data('./test_samples.csv')
-prompts = prompts[5:] # 6번부터 10번만 적용
+prompts = [prompts[9]]   # 10번만 적용
 
 # 질문 원본
-print('='*100, '질문 원본')
-for prompt in prompts:
-    print(prompt,'\n')
+print('질문 원본','='*100,'\n')
+print(prompts[0],'\n')
 
 
 ## 기존의 질문을 "문제유형:핵심질문"으로 변형한 것 -> langchain 사용
-print('='*100, '문제유형:핵심질문')
+print('문제유형:핵심질문','='*100,'\n')
 queries = extract_question_queries(prompts)
 for query in queries:
     print(query,'\n')
 
 
-## 객관식 보기만을 추출한 것
-print('='*100, '객관식 보기만')
-choices = get_choices(prompts)
-for choice in choices:
-    print(choice,'\n')
-
-
 ## 질문에서 키워드를 추출한 것 -> langchain 사용
-print('='*100, '키워드')
+print('키워드','='*100,'\n')
 information = extract_question_keywords(prompts)
 for info in information:
     print(info['keywords'],'\n')
 
 
-
-
-# 예시 키워드로 Wikipedia에서 검색
-keywords = ['jurisprudence', 'law', 'philosophy']
+# 키워드로 Wikipedia에서 검색
+keywords = information[0]['keywords']
 articles = get_wikipedia_articles(keywords)
 
+
 ## 검색된 문서 출력
-print('='*100, '검색된 문서 정보')
-for article in articles:
-    print(f"Title: {article['title']}")
-    print(f"Snippet: {article['snippet']}")
-    print(f"URL: {article['url']}")
-    print()
+print('검색된 문서 정보','='*100, '\n')
+article = articles[0]
+print(f"Title: {article['title']}")
+print(f"Snippet: {article['snippet']}")
+print(f"URL: {article['url']}")
+print()
 
 
-
-
-
-# json 파일로 저장
-print('='*100, 'document_structure.json 파일 확인')
-url = 'https://en.wikipedia.org/?curid=26364'
+# json 파일 구조 출력
+print('document_structure.json 파일 확인','='*100, '\n')
+url = article['url']
 document_structure = parse_wikipedia_page(url)
 
 file_path = './document_structure.json'
 with open(file_path, 'w', encoding='utf-8') as f:
     json.dump(document_structure, f, ensure_ascii=False, indent=4)
+
